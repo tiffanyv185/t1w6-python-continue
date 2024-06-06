3878fdf97a16697d00931623dbd9d0e969afc36b
tiffanyv185 <tiffanyv185@gmail.com>
2024-06-06 20:18:16 +1000

    added sequential flow and readme

---
+++ README.md
@@ -0,0 +1,27 @@
+## Control Flow
+
+Order in which lines of code are exectued in a program.
+
+# Sequential control flow
+
+Execution of code statements one after another, in the order they appear in the program
+
+# Conditional control flow / control flow
+Execution of code statements based on some input
+(if, elif, else)
+
+# Boolean Data Type
+Data type that has two values: True and False. Boolean values are used to control the flow of the program.
+
+# Comparison Operators / Rational Operators
+Decide the relationship between the operands. Result of conparison is a boolean value (True/False)
+
+Always finish conitions with ":"
+
+# if, elif, else
+Simplest form of AI
+
+# pass
+Does nothing, just... passes
+
+# Boolean Operators
---
+++ sequential_flow.py
@@ -0,0 +1,53 @@
+# # print("hello, welcome to programming with Phyton")
+
+# # a = 10
+# # b = 5
+# # result = a + b
+
+# # print(f"The result of adding {a} and {b} is {result}")
+
+# # print("thank you")
+
+# a = 5
+# y = 7
+# z = 9
+
+# print(y < z)
+
+
+# today = "Tuesday"
+
+# if today == "Monday":
+#     print("Set an alarm for 5am")
+# elif today == "Tuesday":
+#     print("Set an alarm for 6am")
+# elif today == "Wednesday":
+#     print("Set alarm for 7am")
+
+
+# temperature = 30
+
+# if temperature > 35:
+#     print("its hot outside")
+# elif temperature < 15:
+#     print("its cold outside")
+# else:
+#     print("The weather is mild")
+
+
+# a = True
+# b = False
+
+# result = not b
+# print(result)
+
+age = 14
+has_permission = True
+
+if age >= 18:
+    if has_permission:
+        print("Access granted.")
+    else:
+        print("Access denied.")
+else:
+        print("Access denied.")
